2e158985cdc6b078de6e355ad305acab9e4385c2
Craig Scott <cscott_taakan@yahoo.com>
2018-09-22 11:04:16 -0700

    Template extension

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,15 @@ from flask import render_template
 @app.route('/index')
 
 def index():
-	user = {'username' : 'Baby'}
-	posts = [
-		{
-			'author' : { 'username' : 'John' },
-			'body' : 'Beautiful day in Portland'
-		},
-		{
-			'author' : { 'username' : 'Sandy'}
-			'body' : 'The Avengers movie was so cool!'
-		}]
-	return render_template('index.html', user=user , posts = posts)
-
+        user = {'username' : 'Craig'}
+        posts = [
+                        {
+				'author' : { 'username' : 'John' },
+				'body' : 'Beautiful day in Portland'
+			},
+			{
+				'author' : { 'username' : 'Sandy'},
+				'body' : 'The Avengers movie was so cool!'
+			}
+		]
+        return render_template('index.html', user=user , posts = posts)
